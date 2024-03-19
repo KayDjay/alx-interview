02476690cd01735a186600365f609f0560326a31
kaydjay <khadijatrasaq@gmail.com>
2024-03-19 15:42:09 +0100

    Amended Code

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,7 +4,8 @@
 
 def minOperations(n):
     """
-    Calculate the minimum number of operations required to achieve a given number of characters.
+    Calculate the minimum number of operations required to achieve a given
+    number of characters.
 
     Args:
         n (int): The target number of characters.
@@ -14,15 +15,15 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     while n > 1:
         if n % divisor == 0:
             operations += divisor
             n //= divisor
         else:
             divisor += 1
-    
+
     return operations
