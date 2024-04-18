0cb3b0a1af34da647c8d27b5cb8082baa04ee06d
Khadijatrasaq001@gmail.com-at-381492146542 <Khadijatrasaq001@gmail.com>
2024-04-19 00:11:27 +0100

    Updated task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,33 @@
 #!/usr/bin/node
-
-const axios = require('axios');
-
+const request = require('request');
 /**
- * Fetches and logs the names of characters from a Star Wars movie.
+ * Retrieves and prints the names of characters from a Star Wars movie.
  * @param {number} movieId - The ID of the Star Wars movie.
- * @returns {Promise<void>} - A promise that resolves once all character names are logged.
  */
-async function getMovieCharacters(movieId) {
-    try {
-        const movieResponse = await axios.get(`https://swapi.dev/api/films/${movieId}/`);
-        const characterUrls = movieResponse.data.characters;
-        
-        for (const url of characterUrls) {
-            try {
-                const characterResponse = await axios.get(url);
-                console.log(characterResponse.data.name);
-            } catch (error) {
-                console.error(`Failed to fetch character data: ${error}`);
-            }
-        }
-    } catch (error) {
-        console.error(`Failed to fetch movie data for movie ID ${movieId}: ${error}`);
-    }
-}
+getMovieCharacters(movieId);
+
+const request = require('request');
 
 const movieId = process.argv[2];
-if (!movieId) {
-    console.error('Usage: node script.js <movie_id>');
-    process.exit(1);
-}
 
-getMovieCharacters(movieId);
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else {
+        const film = JSON.parse(body);
+        const characters = film.characters;
+
+        characters.forEach((characterUrl) => {
+            request(characterUrl, (error, response, body) => {
+                if (error) {
+                    console.error('Error:', error);
+                } else {
+                    const character = JSON.parse(body);
+                    console.log(character.name);
+                }
+            });
+        });
+    }
+});
