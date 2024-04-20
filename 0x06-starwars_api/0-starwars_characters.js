397dc5674a10a046c59f7fadd61232c8050bdf8e
KayDjay <Khadijatrasaq@gmail.com>
2024-04-20 21:30:52 +0100

    Starwars task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,42 @@
 #!/usr/bin/node
-
-/**
- * Retrieves and prints the names of characters from a Star Wars movie.
- * @param {number} movieId - The ID of the Star Wars movie.
- */
-
+// Import the necessary modules
 const request = require('request');
 
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const film = JSON.parse(body);
-    const characters = film.characters;
-
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
+function requestSync (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ response, body });
+      }
     });
+  });
+}
+
+async function getMovieData (filmId) {
+  try {
+    const movieUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}/`;
+
+    const { response, body } = await requestSync(movieUrl);
+
+    if (response.statusCode === 200) {
+      const filmData = JSON.parse(body);
+      const characters = filmData.characters;
+
+      for (const url of characters) {
+        const { response: charResponse, body: charBody } = await requestSync(url);
+        if (charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
   }
-});
+}
+
+const filmId = process.argv[2];
+
+getMovieData(filmId);
