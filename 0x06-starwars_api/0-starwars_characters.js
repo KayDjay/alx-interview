c16ad95a8f60a423462386eab433155b29d3a87d
KayDjay <Khadijatrasaq@gmail.com>
2024-04-20 17:43:53 +0100

    Starwars task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,30 +4,29 @@
  * Retrieves and prints the names of characters from a Star Wars movie.
  * @param {number} movieId - The ID of the Star Wars movie.
  */
-getMovieCharacters(movieId);
 
 const request = require('request');
 
 const movieId = process.argv[2];
 
-const url = `https://swapi.dev/api/films/${movieId}/`;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else {
-        const film = JSON.parse(body);
-        const characters = film.characters;
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-        characters.forEach((characterUrl) => {
-            request(characterUrl, (error, response, body) => {
-                if (error) {
-                    console.error('Error:', error);
-                } else {
-                    const character = JSON.parse(body);
-                    console.log(character.name);
-                }
-            });
-        });
-    }
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+        } else {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  }
 });
