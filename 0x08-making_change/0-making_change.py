4edaced65becaa9717c49397a5b2990e5d65344b
KayDjay <Khadijatrasaq@gmail.com>
2024-05-02 06:52:36 +0100

    Fixing code

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -23,9 +23,10 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for coin in coins:
-        for i in range(1, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
 
     if dp[total] == float('inf'):
         return -1
