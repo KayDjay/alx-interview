1cc02c98e55f1674d8d3d3bf8c8b53d23f562841
KayDjay <Khadijatrasaq@gmail.com>
2024-05-02 07:04:30 +0100

    fixing makechange checker's error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,8 +17,8 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    if total > 0 and len(coins) == 0:
-        return -1
+    # if total > 0 and len(coins) == 0:
+    #     return -1
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
